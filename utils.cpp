49684d09aa33e3919807243ba6004aa3f97905bf
Hunter Medeiros <huntermedeiros08@gmail.com>
2026-09-15 14:28:40 -0500

    Display menu fix

--- utils.cpp
+++ utils.cpp
@@ -8,7 +8,7 @@ void displayMenu(std::string szMenu[], int iLength) {
     std::cout << szMenu[0] << std::endl
               << szBreakMessage;
     for (int i = 1; i < iLength; i++) // Print each menu string
-        std::cout << i << ". " << szMenu[i-1] << std::endl;
+        std::cout << i << ". " << szMenu[i] << std::endl;
     std::cout << "-1. Exit \n\n"; // Print exit string
 }
 
