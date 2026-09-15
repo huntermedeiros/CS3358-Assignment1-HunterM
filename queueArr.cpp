056c73a0d75a37e028ae95cf9f10ab633eb831ae
Hunter <huntermedeiros08@gmail.com>
2026-09-14 22:42:48 -0500

    function definitions

--- queueArr.cpp
+++ queueArr.cpp
@@ -1,2 +1,38 @@
 #include "utils.h"
 #include "queue.h"
+
+        queueArr::queueArr() {
+
+        }
+        queueArr::~queueArr() {
+            
+        }
+
+        queueArr::queueArr(int iSize) {
+
+        }
+
+        void queueArr::enqueue() {
+
+        }
+
+
+        void queueArr::dequeue() {
+
+        }
+
+        void queueArr::peek() {
+
+        }
+
+        bool queueArr::isEmpty() {
+            return false;
+        }
+
+        bool queueArr::isFull() {
+            return false;
+        } 
+
+        int queueArr::currentSize() {
+            return 0;
+        }
--- queuePtr.cpp
+++ queuePtr.cpp
@@ -1,2 +1,33 @@
 #include "utils.h"
 #include "queue.h"
+
+        queuePtr::queuePtr() {
+
+        }
+        queuePtr::~queuePtr() {
+
+        }
+
+        void queuePtr::enqueue() {
+
+        }
+
+        void queuePtr::dequeue() {
+
+        }
+
+        void queuePtr::peek() {
+
+        }
+
+        bool queuePtr::isEmpty() {
+            return false;
+        }
+
+        bool queuePtr::isFull() {
+            return false;
+        } 
+
+        int queuePtr::currentSize() {
+            return 0;
+        }
